0b06400d74d6955417fbe0bf89babf736a5350c1
sebastiankoster <142959691+sebastiankoster@users.noreply.github.com>
2024-01-23 10:50:55 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -2,7 +2,7 @@
 
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	public static void main(final String[] args) boolean found = false;
+//	public static void main(final String[] args) 
 int i = 2; 
 String chunk = new String();
 double num;
