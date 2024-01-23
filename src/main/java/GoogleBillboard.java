d049d32788df940f56e556597111d6c5ac7d74b9
sebastiankoster <142959691+sebastiankoster@users.noreply.github.com>
2024-01-23 10:53:16 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -2,19 +2,17 @@
 
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-//	public static void main(final String[] args) 
-int i = 2; 
+//	public static void main(final String[] args)  
 String chunk = new String();
 double num;
 
-while(!found){
+for(int i = 2; i >-1; i++){
     chunk = e.substring(i,i+10);
     num = Double.parseDouble(chunk);
     if (isPrime(num)){
       System.out.println(num);
-      found = true;
+      i=-3;
     }
-    i++;
  }
 }
 
